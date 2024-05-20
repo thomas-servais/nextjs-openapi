f657b5267174d93b2b6bdf10f0ee9b52cfbdbffc
Thomas Servais <thomas.servais@servais-consulting.com>
2024-05-21 01:28:28 +0200

    try with api folder

--- apps/api/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig, type Options } from "tsup";
-
-export default defineConfig((options: Options) => ({
-  entryPoints: ["src/**/*.ts"],
-  clean: true,
-  format: ["cjs"],
-  ...options,
-}));
