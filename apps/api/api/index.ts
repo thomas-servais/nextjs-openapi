1aa0586a80d3c010987c51e79a12b5a5845f863b
Thomas Servais <thomas.servais@servais-consulting.com>
2024-05-21 14:33:12 +0200

    multipath debug

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,45 +1,23 @@
 import {createServer} from './server';
-import { initialize } from 'express-openapi';
 
-import { setupSwagger } from './swagger';
 import { Application } from 'express';
 import path  from 'path';
 import fs  from 'fs';
-export const app: Application = createServer()
+
+const app: Application = createServer()
 
 const logs: any[] = []
+logs.push('[START] current folder '+process.cwd());
+const target = path.join(process.cwd(), './apps/api/api')
+logs.push('[START] target folder '+target);
 
-console.log('current folder '+process.cwd())
-const absolutePath = path.resolve(process.cwd(), './apps/api/api/api-definition-base.yml');
-console.log('absolutePath '+absolutePath)
-const exists = fs.existsSync(absolutePath)
-const exists2 = fs.existsSync(path.resolve(process.cwd(), './api/api-definition-base.yml'))
-const exists3 = fs.existsSync(path.resolve(process.cwd(), './api-definition-base.yml'))
-logs.push('absolutePath test '+absolutePath+' exists='+exists+' exists2='+exists2+' exists3='+exists3)
-try {
+const folderItems = fs.readdirSync(target);
+folderItems.forEach(item => {
+  logs.push('[ITEM] found ' + item+' in '+target);
+});
 
-initialize({
-            app,
-            docsPath: "/api-definition",
-            exposeApiDocs: true,
-            apiDoc: "./apps/api/api/api-definition-base.yml",
-            paths: "./apps/api/api/paths",
-          }).then( openapi => {
-            logs.push('openapi initialized')
-            setupSwagger(app, openapi.apiDoc);
-          }).catch( err => {
-            logs.push('error initializing openapi '+err)
-            console.log('error initializing openapi', err)
-          })
-        } catch(err) {
-          logs.push('error reading apiDoc '+err)
-          console.log('error reading apiDoc', err)
-        }
-
-        app.get('*', function(req, res){
-          console.log('404 on '+req.url)
-          logs.push('404 on '+req.url)
-          res.status(404).json({url: req.url, logs: logs});
-        });
+app.get('*', function(req, res){
+  res.json({url: req.url, logs: logs});
+});
 
 export default app;
