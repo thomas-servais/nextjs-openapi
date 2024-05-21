a51bd3f578fa0faa84c4f1aee5c4fa9b19f0d374
Thomas Servais <thomas.servais@servais-consulting.com>
2024-05-21 13:14:11 +0200

    fix paths

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -3,15 +3,28 @@ import { initialize } from 'express-openapi';
 
 import { setupSwagger } from './swagger';
 import { Application } from 'express';
-
+import path  from 'path';
+import fs  from 'fs';
 export const app: Application = createServer()
 
+console.log('current folder '+process.cwd())
+const absolutePath = path.resolve(process.cwd(), './apps/api/api/api-definition-base.yml');
+console.log('absolutePath '+absolutePath)
+try {
+const apiDocContent = fs.readFileSync(absolutePath, 'utf8');
+console.log(apiDocContent)
 initialize({
             app,
             docsPath: "/api-definition",
             exposeApiDocs: true,
-            apiDoc: "./dist/api/api-definition-base.yml",
-            paths: "./dist/api/paths",
+            apiDoc: "/var/task/apps/api/api/api-definition-base.yml",
+            paths: "/var/task/apps/api/api/paths",
           }).then( openapi => {
             setupSwagger(app, openapi.apiDoc);
+          }).catch( err => {
+            console.error('error initializing openapi', err)
           })
+        } catch(err) {
+          console.error('error reading apiDoc', err)
+        }
+export default app;
--- apps/api/api/server/index.ts
+++ apps/api/api/server/index.ts
@@ -5,6 +5,8 @@ const PORT = 3002;
 export function createServer(): Application {
     const app = express();
 
+    app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
+
     app.listen(PORT, () => {
       console.log('Server is running on http://localhost:3002');
   });
